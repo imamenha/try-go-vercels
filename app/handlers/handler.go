99c4ff1397326243a4ee8636fb453b7adfaeb9f7
imamenha <imamnurhadianto@gmail.com>
2024-07-19 22:01:46 +0700

    add endpoint test

--- app/handlers/handler.go
+++ app/handlers/handler.go
@@ -16,8 +16,13 @@ func NewHandler(service *services.Service) *Handler {
 
 func (h *Handler) RegisterRoutes(app *fiber.App) {
 	app.Get("/", h.Home)
+	app.Get("/api/test", h.Test) // Pastikan rute ini ada
 }
 
 func (h *Handler) Home(c *fiber.Ctx) error {
 	return c.SendString("Hello, World!")
+}
+
+func (h *Handler) Test(c *fiber.Ctx) error {
+	return c.SendString("This is a test endpoint!")
 }
